0995a518f1a089d2bc14531501f6b3e90df61758
uriel.martinez <uriel.martinez@inateltech.com>
2025-07-10 22:13:52 -0600

    configuracion de use efect

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,11 +5,11 @@ export default function ProductList() {
 
   const { products, loading, error } = useProducts();
   
-  if (loading) return <p>Cargando productos...</p>
   if (error) return <p>Error: {error}</p>
-
+  
   return (
     <div className="productList">
+       { loading && <p>Cargando productos...</p> }
 
         {products.map(product => (
             <ProductItem product={ product } key={product.id}/>
--- src/useContext/SearchProducts.jsx
+++ src/useContext/SearchProducts.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 import { SearchBar } from "../components/SearchBar";
 import useProductSearch from "../customHooks/useProductSearch";
 import '../styles/pages/SearchProducts.css'
@@ -6,15 +6,20 @@ import { SearchProductItem } from "../components/SearchProductItem";
 export default function SearchProducts() {
   const [query, setQuery] = useState("");
   const { results, loading } = useProductSearch(query);
+  const [ mensaje, setMensaje ] = useState("Busquedas sugeridas: ");
   console.log("obteniedo el resultado en el padre", query);
   console.log("este es el result ", results);
-
+  useEffect(() => {
+    if (query !== "" && !results.length && !loading) {
+      setMensaje("No se encontraron resultados de este producto");
+    }
+  }, [query, results, loading]);
   return (
     <>
       <SearchBar onSearch={setQuery} />
       <div className="searchProducts">
         {loading && <div> Buscando los productos </div>}
-        { (!results.length && !loading) && <div> No hay productos de esta busqueda </div> }
+        { (!results.length && !loading) && <div> {mensaje} </div> }
         {results.map((prod) => {
           return <SearchProductItem key={prod.id} product={prod}/>;
         })}
